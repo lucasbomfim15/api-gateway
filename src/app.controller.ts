253061ab84710daea999669518b41c8bdab419aa
lucasbomfim15 <bmfmlucas@gmail.com>
2025-01-09 16:58:15 -0300

    controllers routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,12 @@
 import {
   Body,
   Controller,
+  Get,
   Logger,
+  Param,
   Post,
+  Put,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -12,8 +16,10 @@ import {
   Transport,
 } from '@nestjs/microservices';
 import CriarCategoriaDTO from './dtos/criar-categoria.dto';
+import { Observable } from 'rxjs';
+import AtualizarCategoriaDTO from './dtos/atualizar-categoria.dto';
 
-@Controller('api/v1')
+@Controller('api/v1/categorias')
 export class AppController {
   private logger = new Logger(AppController.name);
 
@@ -29,12 +35,27 @@ export class AppController {
     });
   }
 
-  @Post('categorias')
+  @Post()
   @UsePipes(ValidationPipe)
-  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDTO) {
-    return await this.clientAdminBackend.emit(
-      'criar-categoria',
-      criarCategoriaDto,
-    );
+  criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDTO) {
+    this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto);
+  }
+
+  @Get()
+  consultarCategorias(@Query('idCategoria') _id: string): Observable<any> {
+    const id = _id && _id.trim() !== '' ? _id : null; // Evitar strings vazias
+    return this.clientAdminBackend.send('consultar-categorias', id);
+  }
+
+  @Put('/:_id')
+  @UsePipes(ValidationPipe)
+  atualizarCategoria(
+    @Body() atualizarCategoriaDto: AtualizarCategoriaDTO,
+    @Param('_id') _id: string,
+  ) {
+    this.clientAdminBackend.emit('atualizar-categoria', {
+      _id,
+      categoria: atualizarCategoriaDto,
+    });
   }
 }
